0b70e223c833f70e773431d35903a3584d63ba4d
Jens Ulrich <jensj27@gmail.com>
2021-02-24 11:32:52 -0800

    added a readme tab, deleted old comments

--- ui.R
+++ ui.R
@@ -4,35 +4,21 @@ library(shinythemes)
 
 tib <- as_tibble(read.csv("Gardens.csv"))
 
-ui <- fluidPage(theme = shinytheme("cerulean"),
-  # *Input functions
-#  sliderInput(inputId = "num",
-#              label = "choose a number",
-#              value = 25, min = 1, max = 100),
-  # *Output functions
-#  plotOutput("hist"),
-
-  # Input functions 
-  selectizeInput('var1', 'Select Province', 
-                 choices = c("choose" = "", levels(tib$Province))),
-  # Output functions
-  tableOutput("table")
-)
-
-
-
-
-#shinyApp(
-#  ui <- fluidPage(theme = shinytheme("yeti"), # can search for shiny apps online
-#                  navbarPage(
-#                    "Canadian Crop Wild Relative Inventory", # name of the app
-#                    tabPanel("Navbar 1",
-#                             sidebarPanel(selectizeInput('var1', 'Select Province', choices = c("choose" = "", levels(tib$Province)))
-#                             ), # sidebarPanel
-#                             mainPanel(
-#                               tableOutput("table")
-#                             ) # mainPanel
-#                    )
-#                  )
-#  ),
-  
+ui <- fluidPage(theme = shinytheme("yeti"),
+        navbarPage("Canadian Crop Wild Relative Inventory", # name of the app
+          tabPanel("README" 
+            # First tab could display information about the project and app purposes
+          ),
+          tabPanel("BGCI Gardens", # Panel 2, search through gardens
+            sidebarPanel(
+              # Input functions 
+              selectizeInput('var1', 'Select Province', # Filter all gardens by province
+                             choices = c("choose" = "", levels(tib$Province))),
+            ), # sidebarPanel
+            mainPanel(  
+              # Output functions
+              tableOutput("table") # Table of Gardens
+            ) # mainPanel 
+          ) # tabPanel "BGCI Gardens"
+        ) # navbarPage
+) # fluidPage
