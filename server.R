d232cb0fd8b100517959fa02765153cd514736f0
Jens Ulrich <jensj27@gmail.com>
2021-02-24 11:21:30 -0800

    pasted CWR inputs and outputs into ui and server

--- server.R
+++ server.R
@@ -1,7 +1,18 @@
 library(tidyverse)
 library(shiny)
 
+tib <- as_tibble(read.csv("Gardens.csv"))
+
 server <- function(input, output){
-  output$hist <- renderPlot({ 
-    hist(rnorm(input$num)) })
+#  output$hist <- renderPlot({ 
+#    hist(rnorm(input$num)) })
+  tab <- reactive({ # <-- Reactive function here
+    tib %>% 
+      filter(Province == input$var1)
+  })
+  
+  output$table <- renderTable({
+      tab()
+    }
+  )
 }
--- ui.R
+++ ui.R
@@ -1,11 +1,38 @@
 library(tidyverse)
 library(shiny)
+library(shinythemes)
 
-ui <- fluidPage(
+tib <- as_tibble(read.csv("Gardens.csv"))
+
+ui <- fluidPage(theme = shinytheme("cerulean"),
   # *Input functions
-  sliderInput(inputId = "num",
-              label = "choose a number",
-              value = 25, min = 1, max = 100),
+#  sliderInput(inputId = "num",
+#              label = "choose a number",
+#              value = 25, min = 1, max = 100),
   # *Output functions
-  plotOutput("hist")
+#  plotOutput("hist"),
+
+  # Input functions 
+  selectizeInput('var1', 'Select Province', 
+                 choices = c("choose" = "", levels(tib$Province))),
+  # Output functions
+  tableOutput("table")
 )
+
+
+
+
+#shinyApp(
+#  ui <- fluidPage(theme = shinytheme("yeti"), # can search for shiny apps online
+#                  navbarPage(
+#                    "Canadian Crop Wild Relative Inventory", # name of the app
+#                    tabPanel("Navbar 1",
+#                             sidebarPanel(selectizeInput('var1', 'Select Province', choices = c("choose" = "", levels(tib$Province)))
+#                             ), # sidebarPanel
+#                             mainPanel(
+#                               tableOutput("table")
+#                             ) # mainPanel
+#                    )
+#                  )
+#  ),
+  
