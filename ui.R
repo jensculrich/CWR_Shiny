fc924bc0069f01df106da0f19bc18ecce8322fb4
Jens Ulrich <jensj27@gmail.com>
2021-02-24 16:09:11 -0800

    added tabPanel and sidebar panel text

--- ui.R
+++ ui.R
@@ -6,12 +6,15 @@ tib <- as_tibble(read.csv("Gardens.csv"))
 
 ui <- fluidPage(theme = shinytheme("yeti"),
         navbarPage("Canadian Crop Wild Relative Inventory", # name of the app
-          tabPanel("README" 
+          tabPanel("README", 
+            h1("add title text here"),
+            h4("add descriptive text here")
             # First tab could display information about the project and app purposes
           ),
           tabPanel("BGCI Gardens", # Panel 2, search through gardens
             sidebarPanel(
               # Input functions 
+              h3("BGCI affiliate gardens by province"),
               selectizeInput('var1', 'Select Province', # Filter all gardens by province
                              choices = c("choose" = "", levels(tib$Province))),
             ), # sidebarPanel
