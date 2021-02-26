281f2f2b59918d1af266cb95f1aa838068e53663
Jens Ulrich <jensj27@gmail.com>
2021-02-25 17:12:11 -0800

    added server comments

--- server.R
+++ server.R
@@ -4,14 +4,13 @@ library(shiny)
 tib <- as_tibble(read.csv("Gardens.csv"))
 
 server <- function(input, output){
-#  output$hist <- renderPlot({ 
-#    hist(rnorm(input$num)) })
-  tab <- reactive({ # <-- Reactive function here
+  
+  tab <- reactive({ # <-- Reactive function here, filter table according to input
     tib %>% 
       filter(Province == input$var1)
   })
   
-  output$table <- renderTable({
+  output$table <- renderTable({ # display filtered table
       tab()
     }
   )
