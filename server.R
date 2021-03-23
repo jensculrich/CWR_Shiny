cf6f8b3568776079979c5c679541070fb1f8e628
Jens Ulrich <jensj27@gmail.com>
2021-03-22 17:44:57 -0700

    added example code, select input on ui

--- server.R
+++ server.R
@@ -1,17 +1,61 @@
-library(tidyverse)
-library(shiny)
-
-tibGardens <- as_tibble(read.csv("Gardens.csv"))
+gardens <- as_data_frame(read.csv("Gardens.csv"))
+canada_ecoregions_geojson <- st_read("canada_ecoregions_clipped.geojson", quiet = TRUE)
+canada_provinces_geojson <- st_read("canada_provinces.geojson", quiet = TRUE)
 
 shinyServer(function(input, output){
   
   # gardens is a test output
   output$gardenData <- renderTable({
-    provinceFilter <- subset(tibGardens, tibGardens$Province == input$inProvince)
+    provinceFilter <- subset(gardens, gardens$Province == input$inProvince)
   })
   
   # output$_ <- render_({})
-
+  
+  # example code from local analysis 
+  
+  #species_gap_analyis_by_ecoregion <- full_gap_table %>%
+  # 
+  #filter(species == "user input") %>%
+  #group_by(ECO_NAME) %>%
+  # tally the number of rows in each ecoregion with an existing accession (garden is not NA)
+  #add_tally(!is.na(garden)) %>%
+  #rename("accessions_in_ecoregion" = "n")  %>%
+  #ungroup() %>%
+  # maybe add an if else statement here, so if there's either zero accessions or at least 1?
+  #mutate(total_accessions_for_species = sum(!is.na(garden))) %>%
+  #mutate(accessions_no_geo_data = sum(is.na(ECO_NAME))) %>%
+  #mutate(accessions_with_geo_data = sum(!is.na(ECO_NAME))) %>%
+  #group_by(ECO_NAME) %>%
+  #filter(row_number() == 1) %>%
+  #filter(!is.na(ECO_NAME)) %>%
+  # find number of accessions where ECO_NAME = NA and add this as a universal col
+  # drop rows where ECO_NAME = NA
+  #mutate(binary = ifelse(
+  #  accessions_in_ecoregion > 0, 1, 0)) %>%
+  #ungroup() %>%
+  #mutate(num_native_ecoregions = sum(!duplicated(ECO_NAME))) %>%
+  #mutate(num_covered_ecoregions = sum(binary)) %>%
+  #mutate(perc_ecoregion_range_covered = 
+  #         num_covered_ecoregions / num_native_ecoregions) %>%
+  #dplyr::select(-country, -geometry, -latitude, -longitude, 
+  #              -garden, -X, -province)
+  
+  #species_gap_analyis_by_ecoregion_sf <- tigris::geo_join(canada_eco_subset, species_gap_analyis_by_ecoregion, by = "ECO_NAME")
+  
+  #ZZ <- ggplot() +
+  #  geom_sf(
+  #    aes(fill = binary), 
+  #    color = "gray60", size = 0.1, data = species_gap_analyis_by_ecoregion_sf) +
+  #  coord_sf(crs = crs_string) +
+  #  scale_fill_distiller(palette = "Spectral") +
+  #  guides(fill = FALSE) +
+  #  theme_map() +
+  #  ggtitle("Conservation of Species X Across Native Range in Canadian Botanic Gardens") +
+  #  theme(panel.grid.major = element_line(color = "white"),
+  #        legend.key = element_rect(color = "gray40", size = 0.1),
+  #        plot.title = element_text(color="black", size=10, face="bold.italic", hjust = 0.5)
+  #  )
+  #ZZ
 })
 
 
--- ui.R
+++ ui.R
@@ -1,22 +1,56 @@
-library(tidyverse)
+# do we need to load libraries in both server and ui or are they global?
+
 library(shiny)
 library(shinythemes)
 
-tibGardens <- as_tibble(read.csv("Gardens.csv"))
+library(sf) # the base package manipulating shapes
+library(rgeos)
+library(rgdal) # geo data abstraction library
+library(geojsonio) # geo json input and output
+library(spdplyr) # the `dplyr` counterpart for shapes
+library(rmapshaper) # the package that allows geo shape transformation
+library(magrittr) # data wrangling
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+library(raster)
+library(cartography)
+library(viridis)
+library(tigris)
+
+# do we need to add objects to the environmnet in server and ui or are they global?
+gardens <- as_data_frame(read.csv("Gardens.csv"))
+canada_ecoregions_geojson <- st_read("canada_ecoregions_clipped.geojson", quiet = TRUE)
+canada_provinces_geojson <- st_read("canada_provinces.geojson", quiet = TRUE)
+full_gap_table <- as_data_frame(read.csv("full_gap_table.csv"))
+
 
 ui <- fluidPage(theme = shinytheme("yeti"),
   navbarPage("Canadian Crop Wild Relative Inventory",
     # The BGCI GARDENS IS MEANT TO ACT AS A DUMMY PANEL TO TEST THAT THE APP LAUNCHES 
     tabPanel("BGCI GARDENS",
         sidebarPanel(
-          selectInput("inProvince", "Select a Province", choices = tibGardens$Province)
+          selectInput("inProvince", "Select a Province", 
+                      choices = gardens$Province)
         ),
         mainPanel(
           tableOutput("gardenData")
         )
-    ) # tabPanel("BGCI GARDENS)
-    # add another tabPanel here
+    ), # tabPanel("BGCI GARDENS)
     
-  ) #navbarPage
-) # UI
+    # add another tabPanel here
+    tabPanel("Conduct a CWR Ex Situ Conservation Gap Analysis",
+        sidebarPanel(
+          # need to sort these alphabetically
+          selectInput("selectedCrop", "Select a Crop", 
+                      choices = full_gap_table$crop),
+          # need to filter the list to all CWRs related to the Crop
+          selectInput("selectedCWR", "Select a Crop Wild Relative", 
+                      choices = full_gap_table$species)
+        ) # sidebarPanel
+        # add mainPanel
+      
+    ) # tabPanel("Conduct a CWR Ex Situ Conservation Gap Analysis")
+  ) # navbarPage
+) # ui
  
