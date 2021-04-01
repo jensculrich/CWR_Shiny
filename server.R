d3c26d874399d060cf601f9675cad4f8454520b7
Jens Ulrich <jensj27@gmail.com>
2021-04-01 12:42:26 -0700

    added comments for ideas to complete next

--- server.R
+++ server.R
@@ -14,6 +14,7 @@ canada_provinces_geojson <- st_read("canada_provinces.geojson", quiet = TRUE)
 province_gap_table <- as_data_frame(read.csv("province_gap_table.csv"))
 ecoregion_gap_table <- as_data_frame(read.csv("ecoregion_gap_table.csv"))
 
+
 # Define map projection
 crs_string = "+proj=lcc +lat_1=49 +lat_2=77 +lon_0=-91.52 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"
 
@@ -36,7 +37,7 @@ theme_map <- function(base_size=9, base_family="") { # 3
     )
 }
 
-# write a function to handle no data input (rather than throwing an error I want to display a grey map)
+# edit geometry on gap tables so that the points can be spatially projected
 
 
 shinyServer(function(input, output, session){
